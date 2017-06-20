26578ba9c00ffd9a6236c6da629e58b1059768e2
Joshua Novak <JoshuaNovak919@gmail.com>
2017-06-20 17:26:17 -0400

    Changed install generator to use scss

--- lib/generators/spree_one_page_checkout/install/install_generator.rb
+++ lib/generators/spree_one_page_checkout/install/install_generator.rb
@@ -10,8 +10,8 @@ module SpreeOnePageCheckout
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_one_page_checkout\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_one_page_checkout\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.scss', " *= require spree/frontend/spree_one_page_checkout\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.scss', " *= require spree/backend/spree_one_page_checkout\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
